8b197b9d575381b7020884065b3ef2298abc69dc
Emanuel Garcia <Egc1224@gmail.com>
2022-02-18 14:39:44 -0600

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,42 +1,31 @@
-import { Container, Navbar, Nav } from "react-bootstrap";
+import { Container, Row, Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import FormModal from "./FormModal";
 import TripForm from "./TripForm";
 const Header = ({ title }) => {
-	let currentForm = <TripForm />;
-	return (
-		<>
-			<Navbar
-				bg='dark'
-				variant='dark'
-				className='shadow'
-				style={{ borderRadius: "0px 0px 15px 15px" }}>
-				<Container>
-					<Nav>
-						<Link className='nav-link' to='/'>
-							{title}
-						</Link>
-					</Nav>
-					<Nav className='me-auto'>
-						<Nav.Link>
-							<Link to='/trips' className='nav-link'>
-								Trips
-							</Link>
-						</Nav.Link>
-					</Nav>
-					<Navbar.Toggle />
-					<Navbar.Collapse className='justify-content-end'>
-						<Navbar.Text>
-							<FormModal
-								title='Add Trip'
-								thisForm={currentForm}
-							/>
-						</Navbar.Text>
-					</Navbar.Collapse>
-				</Container>
-			</Navbar>
-		</>
-	);
+  return (
+    <Row>
+      <Navbar
+        bg="dark"
+        variant="dark"
+        className="shadow m-0"
+        style={{ borderRadius: "0px 0px 15px 15px" }}
+      >
+        <Nav>
+          <Link className="nav-link" to="/">
+            {title}
+          </Link>
+        </Nav>
+        <Nav className="me-auto">
+          <Nav.Link>
+            <Link to="/trips" className="nav-link">
+              Trip
+            </Link>
+          </Nav.Link>
+        </Nav>
+      </Navbar>
+    </Row>
+  );
 };
 
 export default Header;
