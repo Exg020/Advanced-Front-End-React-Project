29eefeed33156d79fb471e986173ea36ffded405
Emanuel Garcia <Egc1224@gmail.com>
2022-02-18 14:13:27 -0600

    Updating Main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,15 @@
 import { Row } from "react-bootstrap";
 
 const Main = () => {
-	return (
-		<>
-			<Row className='my-container mx-1 my-4 shadow'>
-				<h1>Main Component</h1>
-			</Row>
-		</>
-	);
+  return (
+    <>
+      <Row className="my-container mx-1 my-4 shadow">
+        <h1 style={{ color: "Black", textAlign: "Center" }}>
+          Welcome To Your Planner
+        </h1>
+      </Row>
+    </>
+  );
 };
 
 export default Main;
