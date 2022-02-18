a1d9321c56d82c691cba2eee298c0744efa29986
Aaron Weegens <a0w0f56@homeoffice.wal-mart.com>
2022-02-18 10:24:11 -0600

    add dumb data

--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -1,40 +1,15 @@
-import { useState, useEffect } from "react";
 import { Row } from "react-bootstrap";
 import ActivityList from "./ActivityList";
 
-const Day = () => {
-	let title = "day 1";
-	let date = "10 sept 2022";
-
-	const [activityList, setActivityList] = useState([]);
-	// GET
-	const getActivities = () => {
-		fetch(`http://localhost:8080/api/getActivities`)
-			.then((response) => response.json())
-			//Then with the data from the response in JSON...
-			.then((activities) => {
-				console.log("Success :", activities);
-				setActivityList([...activities]);
-			})
-			//Then with the error genereted...
-			.catch((error) => {
-				console.error("Error:", error);
-			});
-	};
-
-	useEffect(() => {
-		getActivities();
-	}, []);
-
+const Day = ({ day }) => {
 	return (
 		<>
 			<Row className='my-container mx-1 my-4 py-2'>
 				<h3>
-					{title} --- {date}
+					{day.title} --- {day.date}
 				</h3>
-				<ActivityList activityList={activityList} />
+				<ActivityList activityList={day.activities} />
 			</Row>
-			;
 		</>
 	);
 };
---
+++ src/components/FormModal.jsx
@@ -0,0 +1,29 @@
+import { Modal, Button } from "react-bootstrap";
+import { useState } from "react";
+const FormModal = ({ title, thisForm }) => {
+	const [show, setShow] = useState(false);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+
+	return (
+		<>
+			<Button variant='primary' onClick={handleShow}>
+				Add Trip
+			</Button>
+
+			<Modal
+				show={show}
+				onHide={handleClose}
+				backdrop='static'
+				keyboard={false}>
+				<Modal.Header closeButton>
+					<Modal.Title>{title}</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>{thisForm}</Modal.Body>
+			</Modal>
+		</>
+	);
+};
+
+export default FormModal;
